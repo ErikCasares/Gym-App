8dfddc0ae2f5a16d6f15d4aef7616face7b3a244
Erik Casares <erik@MacBook-Air-de-Erik.local>
2026-04-14 21:15:28 -0300

    14042026

---
+++ gym-app/app/(tabs)/index.tsx
@@ -0,0 +1,67 @@
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import { useRouter } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { obtenerRutinas } from '../../src/storage';
+
+export default function Home() {
+  const router = useRouter();
+  const [rutinas, setRutinas] = useState([]);
+
+  const cargarRutinas = async () => {
+    const data = await obtenerRutinas();
+    setRutinas(data);
+  };
+
+  useEffect(() => {
+    cargarRutinas();
+  }, []);
+
+  return (
+    <View style={{ flex: 1, backgroundColor: '#f5f5f5', padding: 20 }}>
+      
+      <Text style={{ fontSize: 28, fontWeight: 'bold', marginBottom: 20 }}>
+        💪 Mis Rutinas
+      </Text>
+
+      <FlatList
+        data={rutinas}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={{
+            backgroundColor: '#fff',
+            padding: 15,
+            borderRadius: 12,
+            marginBottom: 10,
+            shadowColor: '#000',
+            shadowOpacity: 0.1,
+            shadowRadius: 5,
+            elevation: 3
+          }}>
+            <Text style={{ fontSize: 18, fontWeight: '600' }}>
+              {item.nombre}
+            </Text>
+          </View>
+        )}
+      />
+
+      {/* Botón flotante */}
+      <TouchableOpacity
+        onPress={() => router.push('/crear')}
+        style={{
+          position: 'absolute',
+          bottom: 30,
+          right: 30,
+          backgroundColor: '#000',
+          width: 60,
+          height: 60,
+          borderRadius: 30,
+          justifyContent: 'center',
+          alignItems: 'center',
+          elevation: 5
+        }}
+      >
+        <Text style={{ color: '#fff', fontSize: 30 }}>+</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
